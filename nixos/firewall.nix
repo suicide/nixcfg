da1b99c5da7795bf6ae8afbe0ce5d3fc539c0c1a
suiiii <suiiii@suiiii.rip>
2025-06-11 08:44:51 +0200

    explicitly enable firewall

--- nixos/firewall.nix
+++ nixos/firewall.nix
@@ -6,7 +6,7 @@
     # networking.firewall.allowedTCPPorts = [ ... ];
     # networking.firewall.allowedUDPPorts = [ ... ];
     # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
+    networking.firewall.enable = true;
   };
 }
 
