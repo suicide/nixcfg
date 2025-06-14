1d5768efdb70e8bf7fc61990fb86b81b7846ba2d
suiiii <suiiii@suiiii.rip>
2025-06-14 22:38:47 +0200

    Added direnv

---
+++ home-manager/modules/direnv.nix
@@ -0,0 +1,15 @@
+{ lib, pkgs, config, ... }:
+
+{
+  config = {
+    programs.direnv = {
+      enable = true;
+      enableZshIntegration = true;
+
+      nix-direnv = {
+        enable = true;
+      };
+    };
+  };
+}
+
