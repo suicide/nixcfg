30bc9da0a90b03103fb8103487a8d13febd8e8d8
suiiii <suiiii@suiiii.rip>
2025-06-17 02:02:55 +0200

    Enable rofi theme

--- home-manager/modules/rofi.nix
+++ home-manager/modules/rofi.nix
@@ -1,12 +1,12 @@
 { lib, pkgs, config, ... }:
 let
-  
-  # rofiTheme = pkgs.fetchFromGitHub {
-  #       owner = "newmanls";
-  #       repo = "rofi-themes-collection";
-  #       rev = "ec731cef79d39fc7ae12ef2a70a2a0dd384f9730";
-  #       hash = "";
-  #     };
+
+  rofiTheme = pkgs.fetchFromGitHub {
+        owner = "newmanls";
+        repo = "rofi-themes-collection";
+        rev = "ec731cef79d39fc7ae12ef2a70a2a0dd384f9730";
+        hash = "sha256-96wSyOp++1nXomnl8rbX5vMzaqRhTi/N7FUq6y0ukS8=";
+      };
 in 
 
 {
@@ -15,6 +15,7 @@ in
       enable = true;
       package = pkgs.rofi-wayland;
       terminal = "${lib.getExe pkgs.kitty}";
+      theme = rofiTheme + "/themes/squared-nord.rasi";
     };
   };
 }
