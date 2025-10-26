5950d35fdee7ae7d40de8b08ddf4cbb9b364cf2f
suiiii <suiiii@suiiii.rip>
2025-10-26 15:45:54 +0100

    feat(yazi): Overwrote keymap to delete items

--- home-manager/modules/yazi/default.nix
+++ home-manager/modules/yazi/default.nix
@@ -20,15 +20,23 @@
         };
       };
       keymap = {
-        mgr.prepend_keymap = let
-          numKeys = [1 2 3 4 5 6 7 8 9];
-          asMotion = n: {
-            on = ["${toString n}"];
-            run = "plugin relative-motions ${toString n}";
-            desc = "Move in relative steps";
-          };
-        in
-          map asMotion numKeys;
+        mgr.prepend_keymap =
+          (let
+            numKeys = [1 2 3 4 5 6 7 8 9];
+            asMotion = n: {
+              on = ["${toString n}"];
+              run = "plugin relative-motions ${toString n}";
+              desc = "Move in relative steps";
+            };
+          in
+            map asMotion numKeys)
+          ++ [
+            {
+              # we don't need no trash bin
+              run = "remove --permanently";
+              on = ["d"];
+            }
+          ];
       };
 
       initLua = ./init.lua;
