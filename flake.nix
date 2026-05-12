43265519ea5cd4bf49747038e38d7030fe1d53ba
suiiii <suiiii@suiiii.rip>
2026-05-12 20:08:12 +0200

    chore: Dependency update

--- flake.nix
+++ flake.nix
@@ -48,13 +48,13 @@
 
     # hyprland
     hyprland = {
-      url = "github:hyprwm/Hyprland/v0.54.3";
+      url = "github:hyprwm/Hyprland/v0.55.0";
       # do not change hyprland's nixpkgs to take advantage of cache and not to mess with build
       # inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland-split-monitor-workspaces = {
-      # url = "github:Duckonaut/split-monitor-workspaces/v0.54.3";
-      url = "github:suicide/split-monitor-workspaces/fix-nix-0.54.3";
+      url = "github:zjeffer/split-monitor-workspaces/v0.55.0";
+      # url = "github:suicide/split-monitor-workspaces/fix-nix-0.54.3";
       inputs.hyprland.follows = "hyprland";
     };
 
