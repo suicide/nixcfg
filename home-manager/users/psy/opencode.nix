f66ae09bd33e15f5a6caf79b1abf8d88a7567234
suiiii <suiiii@suiiii.rip>
2026-05-25 23:30:46 +0200

    feat(ai): set models for sub agents

--- home-manager/users/psy/opencode.nix
+++ home-manager/users/psy/opencode.nix
@@ -12,5 +12,30 @@ in {
         docfork.apiKeyFile = "${secrets}/mcp/docfork/api_key";
       };
     };
+
+    programs.opencode = {
+      settings = {
+        agent = let
+          lightweightModel = "opencode-go/deepseek-v4-flash";
+        in {
+          # Built in
+          explore = {
+            model = lightweightModel;
+          };
+          general = {
+            model = lightweightModel;
+          };
+          scout = {
+            model = lightweightModel;
+          };
+
+          # extras
+
+          documentation = {
+            model = lightweightModel;
+          };
+        };
+      };
+    };
   };
 }
