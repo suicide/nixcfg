59c48713e80a34276789857de7ed431566e81c5d
suiiii <suiiii@suiiii.rip>
2026-06-28 19:04:14 +0200

    refactor(zsh): moving from oh-my-zsh to antidote + starship

--- flake.nix
+++ flake.nix
@@ -115,7 +115,7 @@
     inherit (self) outputs;
     systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     forAllSystems = nixpkgs.lib.genAttrs systems;
-    permittedInsecurePackages = [ ];
+    permittedInsecurePackages = [];
     mkSystem = hostname: cfg:
       nixpkgs.lib.nixosSystem {
         specialArgs = {inherit self inputs outputs hostname;};
--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -11,27 +11,13 @@
       syntaxHighlighting.enable = true;
       autosuggestion.enable = true;
 
-      oh-my-zsh = {
+      antidote = {
         enable = true;
-        theme = "robbyrussell";
         plugins = [
-          "copyfile"
-          "cp"
-          # "direnv"
-          "docker"
-          "docker-compose"
-          "extract"
-          "git"
-          "gradle"
-          "kubectl"
-          "mvn"
-          "rsync"
-          "sudo"
-          "vi-mode"
-          "rust"
-          "wd"
-          # "zsh-autosuggestions"
-          # "zsh-syntax-highlighting"
+          "jeffreytse/zsh-vi-mode"
+          "ohmyzsh/ohmyzsh path:plugins/git"
+          "ohmyzsh/ohmyzsh path:plugins/extract"
+          "ohmyzsh/ohmyzsh path:plugins/sudo"
         ];
       };
 
@@ -54,16 +40,64 @@
         "cat" = "bat";
       };
 
-      initContent = ''
-        autoload -Uz edit-command-line
-        zle -N edit-command-line
-        bindkey '^X^E' edit-command-line
-      '';
+      initContent = lib.mkMerge [
+        (lib.mkOrder 540 ''
+          # zsh-vi-mode replaces Ctrl-R in insert mode, so restore the fzf
+          # history widget once it finishes initializing.
+          typeset -ga zvm_after_init_commands
+
+          __cfg_after_zvm_init() {
+            bindkey -M emacs '^R' fzf-history-widget
+            bindkey -M viins '^R' fzf-history-widget
+            bindkey -M vicmd '^R' fzf-history-widget
+          }
+
+          zvm_after_init_commands+=(__cfg_after_zvm_init)
+        '')
+
+        (lib.mkOrder 800 ''
+          # Use fzf-tab for interactive completion while preserving zsh's matcher settings.
+          source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+
+          zstyle ':completion:*' menu no
+          zstyle ':completion:*' matcher-list \
+            'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' \
+            'r:|=*' 'l:|=* r:|=*'
+          zstyle ':completion:*' special-dirs true
+          zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
+          zstyle ':completion:*' use-cache yes
+
+          zstyle ':fzf-tab:*' fzf-flags --bind=tab:down,shift-tab:up
+          zstyle ':fzf-tab:*' switch-group '<' '>'
+        '')
+
+        (lib.mkOrder 900 ''
+          source ${pkgs.fzf}/share/fzf/key-bindings.zsh
+        '')
+
+        ''
+          # Dropped alias-heavy OMZ plugins to re-evaluate later if missed:
+          # copyfile cp docker docker-compose kubectl mvn rsync rust wd gradle
+          autoload -Uz edit-command-line
+          zle -N edit-command-line
+          bindkey '^X^E' edit-command-line
+        ''
+      ];
+    };
+
+    programs.starship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    programs.zoxide = {
+      enable = true;
+      enableZshIntegration = true;
     };
 
     programs.fzf = {
       enable = true;
-      enableZshIntegration = true;
+      enableZshIntegration = false;
       defaultCommand = "fd --type f --hidden --follow --exclude .git";
     };
 
